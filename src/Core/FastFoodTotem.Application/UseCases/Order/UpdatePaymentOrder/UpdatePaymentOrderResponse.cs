bfa7693ec1f87ef71742a090d3525b2e78370820
Marcello Corassin <marcellocorassin@hotmail.com>
2024-01-19 12:44:32 -0300

    Remocao comentario

--- src/Core/FastFoodTotem.Application/UseCases/Order/UpdatePaymentOrder/UpdatePaymentOrderResponse.cs
+++ src/Core/FastFoodTotem.Application/UseCases/Order/UpdatePaymentOrder/UpdatePaymentOrderResponse.cs
@@ -4,12 +4,6 @@ namespace FastFoodTotem.Application.UseCases.Order.UpdatePaymentOrder;
 
 public sealed record UpdatePaymentOrderResponse
 {
-    //public UpdatePaymentOrderResponse(int id, OrderStatus status)
-    //{
-    //    Id = id;
-    //    Status = status.Equals(OrderStatus.AwaitingPayment) ? PaymentStatus.AwaitingPayment : status.Equals(OrderStatus.Canceled) ? PaymentStatus.PaymentDenied : PaymentStatus.PaymentConfirmed;
-    //}
-
     public int Id { get; set; }
     public PaymentStatus Status { get; set; }
 }
